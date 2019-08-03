f401820fc0fc45bbc7d6c269c08511d5da4fc784
HSNBDES33\eocariz <eocariz@hasar.com>
2019-08-02 21:55:48 -0300

    first commit

---
+++ precache-manifest.2ea15d7903dac482e7d53f9f41eba46b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706899ac5e0ba630071e5801b8751aea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c2af1286057e01ad630",
+    "url": "/static/css/main.3ed6bf6e.chunk.css"
+  },
+  {
+    "revision": "6595bf8f97c4b6d961ae",
+    "url": "/static/js/2.19741814.chunk.js"
+  },
+  {
+    "revision": "4c2af1286057e01ad630",
+    "url": "/static/js/main.79906294.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c49feab2e9a8bf9606b42872f127464b",
+    "url": "/static/media/BookMedicalSolid.c49feab2.svg"
+  },
+  {
+    "revision": "a145a343c7d92d9ce9e22f8705335ae4",
+    "url": "/static/media/FlaskSolid.a145a343.svg"
+  },
+  {
+    "revision": "22a84d7caad48257b72136864e93df6b",
+    "url": "/static/media/HospitalRegular.22a84d7c.svg"
+  },
+  {
+    "revision": "5bd8e7281bd1746267d7f30c537a8845",
+    "url": "/static/media/UserInjuredSolid.5bd8e728.svg"
+  },
+  {
+    "revision": "1671e37f7514c3beb78fd454523c76b9",
+    "url": "/static/media/UserSolid.1671e37f.svg"
+  },
+  {
+    "revision": "b087e5fd6114ac5748e7f87fece4b9be",
+    "url": "/static/media/XRaySolid.b087e5fd.svg"
+  },
+  {
+    "revision": "e8417eca20a6156c0158359012791132",
+    "url": "/static/media/logo.e8417eca.png"
+  }
+]);
